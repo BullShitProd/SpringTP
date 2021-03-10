8cdba792fce3f310bb5002817b68c180b75c3989
Corentin Lissillour <corentin.lissillour@hotmail.fr>
2021-03-10 11:54:47 +0100

    Van Service

--- src/main/java/com/example/demoWebService/Service/VanService.java
+++ src/main/java/com/example/demoWebService/Service/VanService.java
@@ -4,9 +4,6 @@ import com.example.demoWebService.Entity.Van;
 import com.example.demoWebService.repository.VanRepository;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import com.example.demoWebService.Entity.Car;
-import com.example.demoWebService.repository.CarRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,24 +31,24 @@ public class VanService {
         return vans;
     }
 
-    @PostMapping("/van")
-    public String addCar(@RequestBody Car car) throws Exception{
-        carRepository.save(car);
+    @PostMapping("/vans")
+    public String addVan(@RequestBody Van van) throws Exception{
+        vanRepository.save(van);
 
-        return "Add car success";
+        return "Add van success";
     }
 
-    @RequestMapping(value = "/cars/{plateNumber}", method = RequestMethod.GET)
+    @RequestMapping(value = "/vans/{plateNumber}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public Car aCar(@PathVariable("plateNumber") String plateNumber) throws Exception{
-        List<Car> cars = new ArrayList<Car>();
+    public Van aVan(@PathVariable("plateNumber") String plateNumber) throws Exception{
+        List<Van> vans = new ArrayList<Van>();
 
-        carRepository.findAll().forEach(cars::add);
+        vanRepository.findAll().forEach(vans::add);
 
-        for(Car car : cars) {
-            if(car.getPlateNumber().equals(plateNumber)) {
-                return car;
+        for(Van van : vans) {
+            if(van.getPlateNumber().equals(plateNumber)) {
+                return van;
             }
         }
 
